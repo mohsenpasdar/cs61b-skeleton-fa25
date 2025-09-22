d599d9bc4bc32144f067180cfbecae2c2ba9a0a0
Mohsen Pasdar <Mohsen.Pasdar@kore.solutions>
2025-09-21 22:33:19 -0400

    proj 1,constructor, addfirst and addlast implementations

---
+++ proj1/src/LinkedListDeque61B.java
@@ -0,0 +1,137 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B {
+    private class Node<T> {
+        private T item;
+        private Node next;
+        private Node prev;
+
+        public Node(T item, Node next, Node prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque61B() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(Object x) {
+        size++;
+        Node originalFirst = sentinel.next;
+        Node newFirst = new Node(x, originalFirst, sentinel);
+        sentinel.next = newFirst;
+        originalFirst.prev = newFirst;
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(Object x) {
+        size++;
+        Node originalLast = sentinel.prev;
+        Node newLast = new Node(x, sentinel, originalLast);
+        originalLast.next = newLast;
+        sentinel.prev = newLast;
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List toList() {
+        return List.of();
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public Object getRecursive(int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Deque61B<Integer> lld = new LinkedListDeque61B<>();
+        lld.addLast(0);   // [0]
+        lld.addLast(1);   // [0, 1]
+        lld.addFirst(-1); // [-1, 0, 1]
+    }
+}
