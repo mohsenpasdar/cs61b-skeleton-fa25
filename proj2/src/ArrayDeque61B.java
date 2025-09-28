cbd24f5c9b117a5571a21bc556c4d58523be6ee9
mohsen <mohsenput2@gmail.com>
2025-09-28 11:48:34 -0400

    proj 2, up to task 6

---
+++ proj2/src/ArrayDeque61B.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B {
+    private int size;
+    private int nextFirst;
+    private T[] items;
+
+    public ArrayDeque61B() {
+        size = 0;
+        nextFirst = 0;
+        items = (T[]) new Object[8];
+    }
+
+    /**
+     * Add {@code x} to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addFirst(Object x) {
+        size++;
+        items[nextFirst] = (T) x;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
+    }
+
+    /**
+     * Add {@code x} to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item to add
+     */
+    @Override
+    public void addLast(Object x) {
+        size++;
+        items[(nextFirst + size) % items.length] = (T) x;
+    }
+
+    /**
+     * Returns a List copy of the deque. Does not alter the deque.
+     *
+     * @return a new list copy of the deque.
+     */
+    @Override
+    public List toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            returnList.addLast((T) get(i));
+        }
+        return returnList;
+    }
+
+    /**
+     * Returns if the deque is empty. Does not alter the deque.
+     *
+     * @return {@code true} if the deque has no elements, {@code false} otherwise.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the size of the deque. Does not alter the deque.
+     *
+     * @return the number of items in the deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Remove and return the element at the front of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public Object removeFirst() {
+        return null;
+    }
+
+    /**
+     * Remove and return the element at the back of the deque, if it exists.
+     *
+     * @return removed element, otherwise {@code null}.
+     */
+    @Override
+    public Object removeLast() {
+        return null;
+    }
+
+    /**
+     * The Deque61B abstract data type does not typically have a get method,
+     * but we've included this extra operation to provide you with some
+     * extra programming practice. Gets the element, iteratively. Returns
+     * null if index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public Object get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        return items[(nextFirst + index + 1) % items.length];
+    }
+
+    /**
+     * This method technically shouldn't be in the interface, but it's here
+     * to make testing nice. Gets an element, recursively. Returns null if
+     * index is out of bounds. Does not alter the deque.
+     *
+     * @param index index to get
+     * @return element at {@code index} in the deque
+     */
+    @Override
+    public Object getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for ArrayDeque61B.");
+    }
+}
--- proj2/tests/ArrayDeque61BTest.java
+++ proj2/tests/ArrayDeque61BTest.java
@@ -3,4 +3,100 @@ import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
 public class ArrayDeque61BTest {
+    @Test
+    /** In this test, we have three different assert statements that verify that addFirst works correctly. */
+    public void addFirstTestBasic() {
+        Deque61B<String> ad1 = new ArrayDeque61B<>();
+
+        ad1.addFirst("back"); // after this call we expect: ["back"]
+        assertThat(ad1.toList()).containsExactly("back").inOrder();
+
+        ad1.addFirst("middle"); // after this call we expect: ["middle", "back"]
+        assertThat(ad1.toList()).containsExactly("middle", "back").inOrder();
+
+        ad1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(ad1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    /** In this test, we use only one assertThat statement. IMO this test is just as good as addFirstTestBasic.
+     *  In other words, the tedious work of adding the extra assertThat statements isn't worth it. */
+    public void addLastTestBasic() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+
+        lld1.addLast("front"); // after this call we expect: ["front"]
+        lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
+        lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    /** This test performs interspersed addFirst and addLast calls. */
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+
+        /* I've decided to add in comments the state after each call for the convenience of the
+            person reading this test. Some programmers might consider this excessively verbose. */
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+
+        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
+
+    @Test
+    /** This test validates the get method implemented by iteration, index i out of range. */
+    public void  testGetOutOfIndex() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(5);
+        lld1.addLast(6);
+        assertThat(lld1.get(-1)).isNull();
+        assertThat(lld1.get(2)).isNull();
+        lld1.addLast(6);
+        assertThat(lld1.get(2)).isEqualTo(6);
+        assertThat(lld1.get(3)).isNull();
+
+    }
+
+    @Test
+    /** This test validates the get method implemented by iteration, index i in range. */
+    public void  testGet() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(5);
+        lld1.addLast(6);
+        lld1.addFirst(3);
+        assertThat(lld1.get(0)).isEqualTo(3);
+        assertThat(lld1.get(1)).isEqualTo(5);
+        assertThat(lld1.get(2)).isEqualTo(6);
+    }
+
+    // Below, you'll write your own tests for LinkedListDeque61B.
+    @Test
+    /** this test makes assertions about isEmpty method. */
+    public void testIsEmpty() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.isEmpty()).isTrue();
+        lld1.addFirst(5);
+        assertThat(lld1.isEmpty()).isFalse();
+    }
+
+    @Test
+    /** this test makes assertion about the size of an empty deque. */
+    public void testSizeZero() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.size()).isEqualTo(0);
+    }
+
+    @Test
+    /** this test makes assertion about the size of an empty deque. */
+    public void testSizeFour() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addFirst(5);
+        lld1.addLast(6);
+        lld1.addFirst(9);
+        lld1.addFirst(0);
+        assertThat(lld1.size()).isEqualTo(4);
+    }
 }
